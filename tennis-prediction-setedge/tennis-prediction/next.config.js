104cbef17fea258edb9d1804e933ce9ac7f4af78
championsalliance89-hue <championsalliance89@gmail.com>
2026-06-04 13:41:08 +0100

    Delete tennis-prediction-setedge/tennis-prediction directory

--- tennis-prediction-setedge/tennis-prediction/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Allow reading from /outputs at build time
-  output: "standalone",
-};
-
-module.exports = nextConfig;
